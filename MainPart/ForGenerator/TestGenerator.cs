0c713d23e388d8697edf8532eb7942f284452897
egorpugachevskyWork <egorpugachevsky@gmail.com>
2022-12-07 12:47:49 +0300

    console program was comleted

--- MainPart/ForGenerator/TestGenerator.cs
+++ MainPart/ForGenerator/TestGenerator.cs
@@ -2,11 +2,7 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MainPart.ForGenerator
 {
--- lab4_final/Program.cs
+++ lab4_final/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MainPart.ForGenerator;
+using MainPart.ForScript;
+
+class Program
+{
+    private static List<string> _namesOfFile = new List<string>() { "A.cs", "B.cs", "Akappuza.cs" }; //{ "Aleksei.cs", "Akappuza.cs", "Gigachad.cs", "Oop.cs", "SPP.cs", "A.cs", "B.cs" };
+    private static string _writePath = "D:\\Studying\\third_course\\СПП\\generated_test";
+
+    static void Main(string[] args)
+    {
+        var scripter = new TestScripter(_namesOfFile, _writePath, 1, 4, 4);
+        scripter.Generate().GetAwaiter().GetResult();
+        Console.WriteLine("Main is finished");
+    }
+
+
+}
